79b29ce2163b06a1f88bef233419593a9d29eb7a
pokemonball34 <pokemonball34@gmail.com>
2018-04-27 14:24:17 -0400

    Another Sketch File
    A base for OOP

--- sketch.py
+++ sketch.py
@@ -1,87 +1,43 @@
-"""
-Simple graphics demo
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-"""
-
-# Import a library of functions called 'pygame'
 import pygame
+from pygame.locals import *
 
-# Initialize the game engine
-pygame.init()
 
-# Define some colors
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
 
-PI = 3.141592653
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
 
-# Set the height and width of the screen
-size = (400, 500)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Cecil is Plagiarizing Code Right Now")
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
 
-# Loop until the user clicks the close button.
-done = False
-clock = pygame.time.Clock()
+    def on_loop(self):
+        pass
 
-# Loop as long as done == False
-while not done:
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-# All drawing code happens after the for loop and but
-# inside the main while not done loop.
-# Clear the screen and set the screen background
-screen.fill(WHITE)
+    def on_render(self):
+        pass
 
-# Draw on the screen a line from (0,0) to (100,100)
-# 5 pixels wide.
-pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
+    def on_cleanup(self):
+        pygame.quit()
 
-# Draw on the screen several lines from (0,10) to (100,110)
-# 5 pixels wide using a loop
-for y_offset in range(0, 100, 10):
-    pygame.draw.line(screen, RED, [0, 10 + y_offset], [100, 110 + y_offset], 5)
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
 
-# Draw a rectangle
-pygame.draw.rect(screen, BLACK, [20, 20, 250, 100], 2)
+        while (self._running):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
 
-# Draw an ellipse, using a rectangle as the outside boundaries
-pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
 
-# Draw an arc as part of an ellipse.
-# Use radians to determine what angle to draw.
-pygame.draw.arc(screen, BLACK, [20, 220, 250, 200], 0, PI / 2, 2)
-pygame.draw.arc(screen, GREEN, [20, 220, 250, 200], PI / 2, PI, 2)
-pygame.draw.arc(screen, BLUE, [20, 220, 250, 200], PI, 3 * PI / 2, 2)
-pygame.draw.arc(screen, RED, [20, 220, 250, 200], 3 * PI / 2, 2 * PI, 2)
-
-# This draws a triangle using the polygon command
-pygame.draw.polygon(screen, BLACK, [[100, 100], [0, 200], [200, 200]], 5)
-
-# Select the font to use, size, bold, italics
-font = pygame.font.SysFont('Calibri', 25, True, False)
-
-# Render the text. "True" means anti-aliased text.
-# Black is the color. This creates an image of the
-# letters, but does not put it on the screen
-text = font.render("My text", True, BLACK)
-
-# Put the image of the text on the screen at 250x250
-screen.blit(text, [250, 250])
-
-# Go ahead and update the screen with what we've drawn.
-# This MUST happen after all the other drawing commands.
-pygame.display.flip()
-
-# This limits the while loop to a max of 60 times per second.
-# Leave this out and we will use all CPU we can.
-clock.tick(60)
-
-# Be IDLE friendly
-pygame.quit()
+if __name__ == "__main__":
+    theApp = App()
+    theApp.on_execute()
